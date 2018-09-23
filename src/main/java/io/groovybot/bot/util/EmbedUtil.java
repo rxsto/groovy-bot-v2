d708f4ad65464d806335f7df22598458f763e4fa
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-23 15:29:12 +0200

    Added utils for Embed

--- src/main/java/io/groovybot/bot/util/EmbedUtil.java
+++ src/main/java/io/groovybot/bot/util/EmbedUtil.java
@@ -1,5 +1,6 @@
 package io.groovybot.bot.util;
 
+import io.groovybot.bot.core.command.CommandEvent;
 import net.dv8tion.jda.core.EmbedBuilder;
 
 import java.awt.*;
@@ -14,7 +15,15 @@ public class EmbedUtil extends SafeMessage {
         return new EmbedBuilder().setDescription(description).setTitle(":x: " + title).setColor(Color.RED);
     }
 
+    public static EmbedBuilder error(CommandEvent event) {
+        return error(event.translate("phrases.unknownerror.title"), event.translate("phrases.unknownerror.description"));
+    }
+
     public static EmbedBuilder info(String title, String description) {
         return new EmbedBuilder().setDescription(description).setTitle(":information_source: " + title).setColor(Color.BLUE);
+    }
+
+    public static EmbedBuilder play(String title, String description) {
+        return new EmbedBuilder().setDescription(description).setTitle(":notes: " + title).setColor(Color.BLUE);
     }
 }
