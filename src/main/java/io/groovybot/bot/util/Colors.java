9929da2de0e7cc8f90ef5c22e185cb62ad64d190
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-23 21:51:56 +0200

    Added Colors

---
+++ src/main/java/io/groovybot/bot/util/Colors.java
@@ -0,0 +1,9 @@
+package io.groovybot.bot.util;
+
+import java.awt.*;
+
+public class Colors {
+
+    public static final Color BLURPLE = new Color(0x7289DA);
+    public static final Color DARK_BUT_NOT_BLACK = new Color(0x2C2F33);
+}
