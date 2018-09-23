8f435295a76c88286c151e29fc254e7d62bc4a7a
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-23 21:50:17 +0200

    Fixed LeaveCommand

--- src/main/java/io/groovybot/bot/commands/music/LeaveCommand.java
+++ src/main/java/io/groovybot/bot/commands/music/LeaveCommand.java
@@ -10,13 +10,14 @@ import io.groovybot.bot.core.command.voice.SameChannelCommand;
 public class LeaveCommand extends SameChannelCommand {
 
     public LeaveCommand() {
-        super(new String[] {"leave"}, CommandCategory.MUSIC, Permissions.everyone(), "Lets the bot leave the channel", "");
+        super(new String[] {"leave", "l"}, CommandCategory.MUSIC, Permissions.everyone(), "Lets the bot leave the channel", "");
     }
 
     @Override
     public Result runCommand(String[] args, CommandEvent event) {
         MusicPlayer player = getPlayer(event.getGuild(), event.getChannel());
-        player.leave();
+        player.stop();
+        player.getLink().disconnect();
         return send(success(event.translate("command.leave.left.title"), event.translate("command.leave.left.description")));
     }
 }
