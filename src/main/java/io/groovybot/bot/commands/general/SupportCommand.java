eeaf9b901bd9f569852fa357be413c45bdb938c2
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-23 21:49:48 +0200

    Added SupportCommand

---
+++ src/main/java/io/groovybot/bot/commands/general/SupportCommand.java
@@ -0,0 +1,18 @@
+package io.groovybot.bot.commands.general;
+
+import io.groovybot.bot.core.command.Command;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+
+public class SupportCommand extends Command {
+    public SupportCommand() {
+        super(new String[] {"support"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you an invite to the official guild", "");
+    }
+
+    @Override
+    public Result run(String[] args, CommandEvent event) {
+        return null;
+    }
+}
