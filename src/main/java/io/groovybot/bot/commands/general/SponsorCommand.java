30cf674ed19a45b0f9ffc1d0cade875b6eac6e37
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-23 21:49:35 +0200

    Added SponsorCommand

---
+++ src/main/java/io/groovybot/bot/commands/general/SponsorCommand.java
@@ -0,0 +1,18 @@
+package io.groovybot.bot.commands.general;
+
+import io.groovybot.bot.core.command.Command;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+
+public class SponsorCommand extends Command {
+    public SponsorCommand() {
+        super(new String[] {"sponsore"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you some information about our sponsor", "");
+    }
+
+    @Override
+    public Result run(String[] args, CommandEvent event) {
+        return null;
+    }
+}
