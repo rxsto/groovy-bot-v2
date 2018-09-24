67565873dcdf8df875a053466812bfbed083c2cb
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-24 22:16:30 +0200

    Added kb, mb, gb etc

--- src/main/java/io/groovybot/bot/commands/general/StatsCommand.java
+++ src/main/java/io/groovybot/bot/commands/general/StatsCommand.java
@@ -9,8 +9,6 @@ import io.groovybot.bot.core.command.permission.Permissions;
 import io.groovybot.bot.util.Colors;
 import net.dv8tion.jda.core.EmbedBuilder;
 
-import java.text.DecimalFormat;
-
 public class StatsCommand extends Command {
     public StatsCommand() {
         super(new String[]{"stats"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you Groovy's current stats", "");
@@ -23,18 +21,24 @@ public class StatsCommand extends Command {
         builder.setTitle(":chart_with_upwards_trend: " + event.translate("command.stats.title"));
         builder.setThumbnail(event.getGuild().getSelfMember().getUser().getAvatarUrl());
 
-        DecimalFormat decimalFormat = new DecimalFormat("###.##");
-
         StringBuilder message = new StringBuilder();
-        message.append(String.format("\n" + event.translate("command.stats.text.playing") + "\n", GroovyBot.getInstance().getMusicPlayerManager().getPlayerStorage().size()));
+        message.append(String.format("\n\n" + event.translate("command.stats.text.playing") + "\n", GroovyBot.getInstance().getMusicPlayerManager().getPlayerStorage().size()));
         message.append(String.format(event.translate("command.stats.text.servers") + "\n", event.getJDA().getGuilds().size()));
         message.append(String.format(event.translate("command.stats.text.members") + "\n", event.getJDA().getUsers().size()));
         message.append(String.format(event.translate("command.stats.text.latency") + "\n", event.getJDA().getPing()));
         message.append(String.format(event.translate("command.stats.text.shards") + "\n", event.getJDA().getShardInfo().getShardId() + 1, event.getJDA().getShardInfo().getShardTotal()));
-        message.append(String.format(event.translate("command.stats.text.memory") + "\n", decimalFormat.format(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()), decimalFormat.format(Runtime.getRuntime().totalMemory())));
+        message.append(String.format(event.translate("command.stats.text.memory") + "\n", humanReadableByteCount(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()), humanReadableByteCount(Runtime.getRuntime().totalMemory())));
 
         builder.setDescription(message);
 
         return send(builder);
+    }
+
+    public static String humanReadableByteCount(long bytes) {
+        int unit = 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = ("KMGTPE").charAt(exp-1) + ("");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 }
