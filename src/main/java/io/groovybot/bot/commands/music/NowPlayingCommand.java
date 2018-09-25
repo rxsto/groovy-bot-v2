01b04c097af6e4b2bcc50861bdc4900200817640
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-25 20:26:56 +0200

    Added NowplayingCommand

---
+++ src/main/java/io/groovybot/bot/commands/music/NowPlayingCommand.java
@@ -0,0 +1,31 @@
+package io.groovybot.bot.commands.music;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import io.groovybot.bot.GroovyBot;
+import io.groovybot.bot.core.audio.MusicPlayer;
+import io.groovybot.bot.core.command.Command;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+import io.groovybot.bot.util.Colors;
+import io.groovybot.bot.util.FormatUtil;
+import net.dv8tion.jda.core.EmbedBuilder;
+
+public class NowPlayingCommand extends Command {
+    public NowPlayingCommand() {
+        super(new String[] {"now", "np", "nowplaying"}, CommandCategory.MUSIC, Permissions.everyone(), "Shows you all information about the current track", "");
+    }
+
+    @Override
+    public Result run(String[] args, CommandEvent event) {
+        MusicPlayer player = GroovyBot.getInstance().getMusicPlayerManager().getPlayer(event.getGuild(), event.getChannel());
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setColor(Colors.DARK_BUT_NOT_BLACK);
+        AudioTrack playingTrack = player.getPlayer().getPlayingTrack();
+        builder.setTitle(String.format(":notes: %s", playingTrack.getInfo().title), playingTrack.getInfo().uri);
+        builder.setThumbnail(FormatUtil.getThumbnail(playingTrack));
+        builder.addField(event.translate("command.now.title"), String.format("**%s:** %s - **%s:** [%s/%s]", event.translate("phrases.text.author"), playingTrack.getInfo().author, event.translate("phrases.text.duration"), FormatUtil.formatTimestamp(playingTrack.getPosition()), FormatUtil.formatTimestamp(playingTrack.getDuration())), false);
+        return send(builder);
+    }
+}
