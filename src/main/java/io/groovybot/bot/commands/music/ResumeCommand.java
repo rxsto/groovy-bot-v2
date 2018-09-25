9cd60d89bda6beef37d85bf6b4e68e019651ae6f
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-25 14:35:42 +0200

    Added Pause and Resume Commands

---
+++ src/main/java/io/groovybot/bot/commands/music/ResumeCommand.java
@@ -0,0 +1,27 @@
+package io.groovybot.bot.commands.music;
+
+import io.groovybot.bot.GroovyBot;
+import io.groovybot.bot.core.audio.MusicPlayer;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+import io.groovybot.bot.core.command.voice.SameChannelCommand;
+
+public class ResumeCommand extends SameChannelCommand {
+    public ResumeCommand() {
+        super(new String[] {"resume"}, CommandCategory.MUSIC, Permissions.everyone(), "Lets you resume the bot", "");
+    }
+
+    @Override
+    public Result runCommand(String[] args, CommandEvent event) {
+        MusicPlayer player = GroovyBot.getInstance().getMusicPlayerManager().getPlayer(event.getGuild(), event.getChannel());
+        if (!player.isPlaying())
+            return send(error(event.translate("phrases.notplaying.title"), event.translate("phrases.notplaying.description")));
+        if (!player.isPaused())
+            return send(error(event.translate("command.resume.already.title"), event.translate("command.resume.already.description")));
+
+        player.resume();
+        return send(success(event.translate("command.resume.title"), event.translate("command.resume.description")));
+    }
+}
