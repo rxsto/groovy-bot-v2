0d6af6b1b08ffe10b607a3923f2b5ba7abbda5f3
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-25 20:26:50 +0200

    Added LoopqueueCommand

---
+++ src/main/java/io/groovybot/bot/commands/music/LoopQueueCommand.java
@@ -0,0 +1,26 @@
+package io.groovybot.bot.commands.music;
+
+import io.groovybot.bot.core.audio.MusicPlayer;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+import io.groovybot.bot.core.command.voice.SameChannelCommand;
+
+public class LoopQueueCommand extends SameChannelCommand {
+
+    public LoopQueueCommand() {
+        super(new String[]{"loopqueue", "lq", "looprepeat", "lr"}, CommandCategory.MUSIC, Permissions.everyone(), "Loops the whole queue", "");
+    }
+
+    @Override
+    public Result runCommand(String[] args, CommandEvent event) {
+        MusicPlayer player = getPlayer(event.getGuild(), event.getChannel());
+        if (!player.getScheduler().isQueueRepeating()) {
+            player.getScheduler().setQueueRepeating(true);
+            return send(success(event.translate("command.queueloop.enabled.title"), event.translate("command.queueloop.enabled.description")));
+        }
+        player.getScheduler().setQueueRepeating(false);
+        return send(success(event.translate("command.queueloop.disabled.title"), event.translate("command.queueloop.disabled.description")));
+    }
+}
