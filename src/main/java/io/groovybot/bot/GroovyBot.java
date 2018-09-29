6b6baca86ca07279d90ca7ba0f3c8cf84a4fe038
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-09-29 14:41:18 +0200

    Added accounce command
    - Fixed premium
    - Added announce command

--- src/main/java/io/groovybot/bot/GroovyBot.java
+++ src/main/java/io/groovybot/bot/GroovyBot.java
@@ -3,6 +3,7 @@ package io.groovybot.bot;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import io.groovybot.bot.commands.general.*;
 import io.groovybot.bot.commands.music.*;
+import io.groovybot.bot.commands.settings.AnnounceCommand;
 import io.groovybot.bot.commands.settings.DjModeCommand;
 import io.groovybot.bot.commands.settings.LanguageCommand;
 import io.groovybot.bot.commands.settings.PrefixCommand;
@@ -317,7 +318,8 @@ public class GroovyBot {
                 new ShuffleCommand(),
                 new RemoveCommand(),
                 new KeyCommand(),
-                new ForcePlayCommand()
+                new ForcePlayCommand(),
+                new AnnounceCommand()
         );
     }
 
