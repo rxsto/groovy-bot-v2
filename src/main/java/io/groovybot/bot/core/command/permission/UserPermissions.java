f7849e1b58ac5d912dc581b9aa20187b1dc80683
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-29 17:10:16 +0200

    Spelling and debug msg

--- src/main/java/io/groovybot/bot/core/command/permission/UserPermissions.java
+++ src/main/java/io/groovybot/bot/core/command/permission/UserPermissions.java
@@ -27,7 +27,6 @@ public class UserPermissions {
         if (guild.getMemberById(user.getEntityId()).getVoiceState().inVoiceChannel())
             if (guild.getMemberById(user.getEntityId()).getVoiceState().getChannel().getMembers().size() == 2)
                 return true;
-        System.out.println("dont work");
         for (Role role : guild.getMemberById(user.getEntityId()).getRoles()) {
             if (role.getName().toLowerCase().contains("dj"))
                 return true;
