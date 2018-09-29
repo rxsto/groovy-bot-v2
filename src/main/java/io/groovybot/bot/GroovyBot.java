01cf9f045bb0a3b852d82e36c93719974583c6ca
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-29 15:09:00 +0200

    Added updateCommand

--- src/main/java/io/groovybot/bot/GroovyBot.java
+++ src/main/java/io/groovybot/bot/GroovyBot.java
@@ -3,6 +3,7 @@ package io.groovybot.bot;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import io.groovybot.bot.commands.general.*;
 import io.groovybot.bot.commands.music.*;
+import io.groovybot.bot.commands.owner.UpdateCommand;
 import io.groovybot.bot.commands.settings.DjModeCommand;
 import io.groovybot.bot.commands.settings.LanguageCommand;
 import io.groovybot.bot.commands.settings.PrefixCommand;
@@ -316,7 +317,8 @@ public class GroovyBot {
                 new MoveCommand(),
                 new RemoveCommand(),
                 new KeyCommand(),
-                new ForcePlayCommand()
+                new ForcePlayCommand(),
+                new UpdateCommand()
         );
     }
 
