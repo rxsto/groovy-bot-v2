e6ffb2e5e7b7ae7424ae2c6737e60f0a7b2ead0c
Oskar Lang <oskar.risto.lang@gmail.com>
2018-09-29 11:57:45 +0200

    Cleanup

---
+++ src/main/java/io/groovybot/bot/commands/music/ForcePlayCommand.java
@@ -0,0 +1,23 @@
+package io.groovybot.bot.commands.music;
+
+import io.groovybot.bot.core.audio.MusicPlayer;
+import io.groovybot.bot.core.command.CommandCategory;
+import io.groovybot.bot.core.command.CommandEvent;
+import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.permission.Permissions;
+import io.groovybot.bot.core.command.voice.SemiInChannelCommand;
+
+public class ForcePlayCommand extends SemiInChannelCommand {
+
+    public ForcePlayCommand() {
+        super(new String[] {"forceplay", "playskip"}, CommandCategory.MUSIC, Permissions.tierTwo(), "Like play but adds the song to the top and skips", "<song>");
+    }
+
+    @Override
+    protected Result executeCommand(String[] args, CommandEvent event, MusicPlayer player) {
+        if (args.length == 0)
+            return sendHelp();
+        player.queueSongs(event, true, false);
+        return null;
+    }
+}
