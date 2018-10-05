770f6ea5ba58ba7ad7b54212d5559a7084ec20ff
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-10-05 11:46:32 +0200

    Fixed shuffle
    - The bot does no leave when the queue is empty
    - Improved Scheduler
    - Added some colors

--- src/main/java/io/groovybot/bot/util/Colors.java
+++ src/main/java/io/groovybot/bot/util/Colors.java
@@ -2,8 +2,15 @@ package io.groovybot.bot.util;
 
 import java.awt.*;
 
+/**
+ * All colors from the Discord press kit: https://discordapp.com/branding
+ */
 public class Colors {
 
     public static final Color BLURPLE = new Color(0x7289DA);
     public static final Color DARK_BUT_NOT_BLACK = new Color(0x2C2F33);
+    public static final Color GREYPLE = new Color(0x99AAB5);
+    public static final Color FULL_WHITE = new Color(0xFFFFFF);
+    public static final Color NOT_QUITE_BLACK = new Color(0x23272A);
+
 }
