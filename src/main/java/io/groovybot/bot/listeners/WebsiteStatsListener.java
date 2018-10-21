33b74378222287c8342b81ed79758039ab10f1d5
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-10-21 11:36:43 +0200

    DB fix

--- src/main/java/io/groovybot/bot/listeners/WebsiteStatsListener.java
+++ src/main/java/io/groovybot/bot/listeners/WebsiteStatsListener.java
@@ -52,13 +52,14 @@ public class WebsiteStatsListener implements Runnable {
     }
 
     private void sendHeartBeat() {
-        GroovyBot.getInstance().getWebsocket().send(WebsocketConnection.parseMessage("heartbeat", new JSONObject().put("state", "alive")).toString());
+        GroovyBot.getInstance().getWebsocket().send(WebsocketConnection.parseMessage("bot","heartbeat", new JSONObject().put("state", "alive")).toString());
+        log.debug("[Websocket] Sending Heartbeat to Server!");
     }
 
     private void updateStats() {
-        if (GroovyBot.getInstance().getWebsocket().isClosed())
+        if (GroovyBot.getInstance().getWebsocket().isClosed() || !GroovyBot.getInstance().getWebsocket().isOpen())
             return;
-        log.debug("[Websocket] Sending Heartbeat to Server!");
-        GroovyBot.getInstance().getWebsocket().send(WebsocketConnection.parseMessage("poststats", WebsocketConnection.parseStats(GroovyBot.getInstance().getLavalinkManager().countPlayers(), GroovyBot.getInstance().getShardManager().getGuilds().size(), GroovyBot.getInstance().getShardManager().getUsers().size())).toString());
+        log.debug("[Websocket] Updating stats!");
+        GroovyBot.getInstance().getWebsocket().send(WebsocketConnection.parseMessage("bot","poststats", WebsocketConnection.parseStats(GroovyBot.getInstance().getLavalinkManager().countPlayers(), GroovyBot.getInstance().getShardManager().getGuilds().size(), GroovyBot.getInstance().getShardManager().getUsers().size())).toString());
     }
 }
