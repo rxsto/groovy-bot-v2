376be48fa13c67ec63babfea80461ab4e2c5082e
Oskar Lang <oskar.risto.lang@gmail.com>
2018-10-29 21:21:07 +0100

    Updated StatsCommand

--- src/main/java/io/groovybot/bot/commands/general/StatsCommand.java
+++ src/main/java/io/groovybot/bot/commands/general/StatsCommand.java
@@ -1,44 +1,39 @@
 package io.groovybot.bot.commands.general;
 
+import com.sun.management.OperatingSystemMXBean;
 import io.groovybot.bot.core.command.Command;
 import io.groovybot.bot.core.command.CommandCategory;
 import io.groovybot.bot.core.command.CommandEvent;
 import io.groovybot.bot.core.command.Result;
 import io.groovybot.bot.core.command.permission.Permissions;
 import io.groovybot.bot.util.Colors;
+import io.groovybot.bot.util.EmbedUtil;
+import io.groovybot.bot.util.FormatUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
+
+import java.lang.management.ManagementFactory;
 
 public class StatsCommand extends Command {
     public StatsCommand() {
         super(new String[]{"stats"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you Groovy's current stats", "");
     }
 
-    public static String humanReadableByteCount(long bytes) {
-        int unit = 1024;
-        if (bytes < unit) return bytes + " B";
-        int exp = (int) (Math.log(bytes) / Math.log(unit));
-        String pre = ("KMGTPE").charAt(exp - 1) + ("");
-        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
-    }
-
     @Override
     public Result run(String[] args, CommandEvent event) {
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
         EmbedBuilder builder = new EmbedBuilder();
         builder.setColor(Colors.DARK_BUT_NOT_BLACK);
         builder.setTitle(":chart_with_upwards_trend: " + event.translate("command.stats.title"));
-        builder.setThumbnail(event.getGuild().getSelfMember().getUser().getAvatarUrl());
-
-        StringBuilder message = new StringBuilder();
-        message.append(String.format("\n\n" + event.translate("command.stats.text.playing") + "\n", event.getGroovyBot().getMusicPlayerManager().getPlayingServers()));
-        message.append(String.format(event.translate("command.stats.text.servers") + "\n", event.getGroovyBot().getShardManager().getGuilds().size()));
-        message.append(String.format(event.translate("command.stats.text.members") + "\n", event.getGroovyBot().getShardManager().getUsers().size()));
-        message.append(String.format(event.translate("command.stats.text.latency") + "\n", event.getJDA().getPing()));
-        message.append(String.format(event.translate("command.stats.text.shards") + "\n", event.getJDA().getShardInfo().getShardId() + 1, event.getJDA().getShardInfo().getShardTotal()));
-        message.append(String.format(event.translate("command.stats.text.memory") + "\n", humanReadableByteCount(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()), humanReadableByteCount(Runtime.getRuntime().totalMemory())));
-        message.append(String.format(event.translate("command.stats.text.threads") + "\n", Thread.getAllStackTraces().size()));
-
-        builder.setDescription(message);
-        sendMessageBlocking(event.getChannel(), builder);
-        return null;
+        builder.addField(event.translate("command.stats.text.playing"), String.format("**%s** %s", event.getBot().getMusicPlayerManager().getPlayingServers(), event.translate("phrases.text.guilds")), true);
+        builder.addField(event.translate("command.stats.text.servers"), String.format("**%s** %s", event.getBot().getShardManager().getGuilds().size(), event.translate("phrases.text.guilds")), true);
+        builder.addField(event.translate("command.stats.text.members"), String.format("**%s** %s", event.getBot().getShardManager().getUsers().size(), event.translate("phrases.text.users")), true);
+        builder.addField(event.translate("command.stats.text.latency"), String.format("**%s**ms", event.getJDA().getPing()), true);
+        builder.addField(event.translate("command.stats.text.shards"), String.format("**%s**/**%s** %s", event.getJDA().getShardInfo().getShardId() + 1, event.getJDA().getShardInfo().getShardTotal(), event.translate("phrases.text.shards")), true);
+        builder.addField(event.translate("command.stats.text.cpu"), String.format("**%s**", Math.round(operatingSystemMXBean.getSystemCpuLoad() * 100) + "%"), true);
+        builder.addField(event.translate("command.stats.text.memory"), String.format("**%s**", FormatUtil.humanReadableByteCount(operatingSystemMXBean.getTotalPhysicalMemorySize() - operatingSystemMXBean.getFreePhysicalMemorySize())), true);
+        builder.addField(event.translate("command.stats.text.threads"), String.format("**%s** %s", Thread.getAllStackTraces().size(), event.translate("phrases.text.threads")), true);
+        builder.addField(event.translate("command.stats.text.uptime"), String.format("**%s**", FormatUtil.parseUptime(System.currentTimeMillis() - event.getBot().getStartupTime())), true);
+        return send(builder);
     }
 }
