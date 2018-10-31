cc4814af3332b8c16d4f7af268299b0af04cfa07
Kuseru <sergej.herdt.sh@gmail.com>
2018-10-31 23:52:30 +0100

    Adding Spotify-Playlist support (still working though...)

---
+++ src/main/java/io/groovybot/bot/core/audio/spotify/request/GetNormalPlaylistRequest.java
@@ -0,0 +1,44 @@
+package io.groovybot.bot.core.audio.spotify.request;
+
+import com.neovisionaries.i18n.CountryCode;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.requests.data.AbstractDataRequest;
+
+import java.io.IOException;
+
+public class GetNormalPlaylistRequest extends AbstractDataRequest {
+
+    private GetNormalPlaylistRequest(final Builder builder) {
+        super(builder);
+    }
+
+    @Override
+    public Playlist execute() throws IOException, SpotifyWebApiException {
+        return new Playlist.JsonUtil().createModelObject(getJson());
+    }
+
+    public static final class Builder extends AbstractDataRequest.Builder<Builder> {
+
+        public Builder(String accessToken) {
+            super(accessToken);
+        }
+
+        public Builder playlistId(String playlistId) {
+            assert (playlistId != null);
+            assert (!playlistId.equals(""));
+            return setPathParameter("playlist_id", playlistId);
+        }
+
+        public Builder market(final CountryCode market) {
+            assert (market != null);
+            return setQueryParameter("market", market);
+        }
+
+        @Override
+        public GetNormalPlaylistRequest build() {
+            setPath("/v1/playlists/{playlist_id}");
+            return new GetNormalPlaylistRequest(this);
+        }
+    }
+}
