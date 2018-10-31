a629674199e7e833b002c382c38fdf55c802922a
Oskar Lang <oskar.risto.lang@gmail.com>
2018-10-31 15:31:36 +0100

    Improved ShardCommand

--- src/main/java/io/groovybot/bot/commands/general/ShardCommand.java
+++ src/main/java/io/groovybot/bot/commands/general/ShardCommand.java
@@ -1,17 +1,16 @@
 package io.groovybot.bot.commands.general;
 
-import io.groovybot.bot.core.command.Command;
-import io.groovybot.bot.core.command.CommandCategory;
-import io.groovybot.bot.core.command.CommandEvent;
-import io.groovybot.bot.core.command.Result;
+import io.groovybot.bot.core.command.*;
 import io.groovybot.bot.core.command.permission.Permissions;
 import io.groovybot.bot.util.Colors;
+import io.groovybot.bot.util.FormatUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
 
 public class ShardCommand extends Command {
 
     public ShardCommand() {
         super(new String[]{"shard", "shards"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you some information about your current shard", "");
+        registerSubCommand(new ListCommand());
     }
 
     @Override
@@ -20,5 +19,18 @@ public class ShardCommand extends Command {
         builder.setColor(Colors.DARK_BUT_NOT_BLACK);
         builder.setDescription("<:online:449207830105554964> " + String.format(event.translate("command.shard.description"), event.getJDA().getShardInfo().getShardId() + 1, event.getJDA().getPing()));
         return send(builder);
+    }
+
+    private class ListCommand extends SubCommand {
+
+        public ListCommand() {
+            super(new String[]{"list", "all"}, Permissions.everyone(), "Lists all shards with information", "");
+        }
+
+        @Override
+        public Result run(String[] args, CommandEvent event) {
+            String message = FormatUtil.parseShardsMessage(event);
+            return send(message);
+        }
     }
 }
