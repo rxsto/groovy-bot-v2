e3dde07469de43505eacedc62baa9cdbd1ce92c4
Oskar Lang <oskar.risto.lang@gmail.com>
2018-11-02 23:56:41 +0100

    Added and Changed Parser

--- src/main/java/io/groovybot/bot/util/FormatUtil.java
+++ src/main/java/io/groovybot/bot/util/FormatUtil.java
@@ -1,17 +1,26 @@
 package io.groovybot.bot.util;
 
+import com.google.common.collect.Lists;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
-import io.groovybot.bot.core.audio.QueuedTrack;
 import io.groovybot.bot.core.command.Command;
 import io.groovybot.bot.core.command.CommandEvent;
 import io.groovybot.bot.core.command.SubCommand;
+import io.groovybot.bot.core.events.command.CommandFailEvent;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -20,10 +29,8 @@ import static io.groovybot.bot.util.EmbedUtil.info;
 
 public class FormatUtil {
 
-    public static String formatTrack(AudioTrack track) {
-        QueuedTrack queuedTrack = ((QueuedTrack) track);
-        AudioTrackInfo trackInfo = queuedTrack.getInfo();
-        return String.format("%s (%s) **Requested by %s**", trackInfo.title, trackInfo.author, formatUserName(queuedTrack.getRequester()));
+    public static String formatTrack(AudioTrack audioTrack) {
+        return String.format("%s (%s)", audioTrack.getInfo().title, audioTrack.getInfo().author);
     }
 
     /**
@@ -70,16 +77,21 @@ public class FormatUtil {
         stringBuilder.append("Command aliases: `").append(Arrays.toString(command.getAliases()).replace("[", "").replace("]", "")).append("`\n");
         stringBuilder.append("Description: `").append(command.getDescription()).append("`").append("\n");
         stringBuilder.append("Usage: `").append(buildUsage(command)).append("`\n");
-        command.getSubCommandAssociations().values().parallelStream().distinct().collect(Collectors.toList()).forEach(subCommand -> stringBuilder.append(buildUsage(subCommand)).append("\n"));
+
+        if (!command.getSubCommandAssociations().isEmpty()) {
+            stringBuilder.append("Subcommands:").append("\n");
+            command.getSubCommandAssociations().values().parallelStream().distinct().collect(Collectors.toList()).forEach(subCommand -> stringBuilder.append(buildUsage(subCommand)).append("\n"));
+        }
+
         return stringBuilder;
     }
 
     private static String buildUsage(Command command) {
         if (command instanceof SubCommand) {
             SubCommand subCommand = ((SubCommand) command);
-            return "g!" + subCommand.getMainCommand().getName() + " " + subCommand.getName() + " " + subCommand.getUsage() + " - " + subCommand.getDescription();
+            return String.format(":white_check_mark: `g!%s %s %s` `%s`", subCommand.getMainCommand().getName(), subCommand.getName(), subCommand.getUsage(), subCommand.getDescription());
         }
-        return "g!" + command.getName() + " " + command.getUsage();
+        return String.format("g!%s %s", command.getName(), command.getUsage());
     }
 
     public static String humanReadableByteCount(long bytes) {
@@ -133,14 +145,84 @@ public class FormatUtil {
         return edit;
     }
 
-    public static String parseShardsMessage(CommandEvent event) {
+    public static EmbedBuilder parseShardsMessage(CommandEvent event) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
         StringBuilder stringBuilder = new StringBuilder();
 
-        event.getBot().getShardManager().getShards().forEach(shard -> {
-
-            stringBuilder.append(String.format("%s. %s - %s - %s - %sms", shard.getShardInfo().getShardId() + 1, event.getBot().getShardManager().getStatus(shard.getShardInfo().getShardId()), shard.getGuilds().size(), shard.getUsers().size(), shard.getPing())).append("\n");
+        Lists.reverse(event.getBot().getShardManager().getShards()).forEach(shard -> {
+            stringBuilder.append(String.format("**%s Shard %s %s » %sms**", event.getBot().getShardManager().getStatus(shard.getShardInfo().getShardId()).toString().equals("CONNECTED") ? "<:online:449207830105554964>" : "<:dnd:449207827660013568>", shard.getShardInfo().getShardId() + 1, event.getBot().getShardManager().getStatus(shard.getShardInfo().getShardId()).toString().equals("CONNECTED") ? "online" : event.getBot().getShardManager().getStatus(shard.getShardInfo().getShardId()).toString(), shard.getPing()));
+            if (event.getBot().getShardManager().getShardsTotal() > 1) stringBuilder.append("\n");
         });
 
-        return stringBuilder.toString();
+        embedBuilder.setDescription(stringBuilder.toString());
+        embedBuilder.setColor(Colors.DARK_BUT_NOT_BLACK);
+        return embedBuilder;
+    }
+
+    public static EmbedBuilder formatWebhookMessage(String type, Event event) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setFooter(event.getJDA().getSelfUser().getName(), event.getJDA().getSelfUser().getAvatarUrl());
+        embedBuilder.setTimestamp(Instant.now());
+
+        switch (type) {
+            case "GUILDJOIN":
+                GuildJoinEvent guildJoinEvent = ((GuildJoinEvent) event);
+                embedBuilder.setTitle(String.format("✅ Joined Guild %s", guildJoinEvent.getGuild().getName()));
+                embedBuilder.setDescription(formatGuildLog(((GuildJoinEvent) event).getGuild()));
+                embedBuilder.setThumbnail(((GuildJoinEvent) event).getGuild().getIconUrl());
+                break;
+            case "GUILDLEAVE":
+                GuildLeaveEvent guildLeaveEvent = ((GuildLeaveEvent) event);
+                embedBuilder.setTitle(String.format("❌ Left Guild %s", guildLeaveEvent.getGuild().getName()));
+                embedBuilder.setDescription(formatGuildLog(((GuildLeaveEvent) event).getGuild()));
+                embedBuilder.setThumbnail(((GuildLeaveEvent) event).getGuild().getIconUrl());
+                break;
+            case "MEMBERJOIN":
+                GuildMemberJoinEvent guildMemberJoinEvent = ((GuildMemberJoinEvent) event);
+                embedBuilder.setTitle(String.format("✅ User %s Joined", formatUserName(guildMemberJoinEvent.getUser())));
+                embedBuilder.setDescription(formatMemberLog(((GuildMemberJoinEvent) event).getMember()));
+                embedBuilder.setThumbnail(((GuildMemberJoinEvent) event).getMember().getUser().getAvatarUrl());
+                break;
+            case "MEMBERLEAVE":
+                GuildMemberLeaveEvent guildMemberLeaveEvent = ((GuildMemberLeaveEvent) event);
+                embedBuilder.setTitle(String.format("❌ User %s Left", formatUserName(guildMemberLeaveEvent.getUser())));
+                embedBuilder.setDescription(formatMemberLog(((GuildMemberLeaveEvent) event).getMember()));
+                embedBuilder.setThumbnail(((GuildMemberLeaveEvent) event).getMember().getUser().getAvatarUrl());
+                break;
+            case "ERROR":
+                CommandFailEvent commandFailEvent = ((CommandFailEvent) event);
+                embedBuilder.setTitle(String.format("⚠ Command '%s' Failed", commandFailEvent.getCommand().getAliases()[0]));
+                embedBuilder.addField("Message", "`" + formatException(commandFailEvent.getThrowable()) + "`", false);
+                embedBuilder.addField("Stacktrace", "```" + formatStacktrace(commandFailEvent.getThrowable()) + "```", false);
+                break;
+        }
+
+        return embedBuilder;
+    }
+
+    public static String formatStacktrace(Throwable throwable) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            if (i > throwable.getStackTrace().length)
+                break;
+            try {
+                out.append(throwable.getStackTrace()[i]).append("\n");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+        }
+        return out.toString();
+    }
+
+    public static String formatException(Throwable throwable) {
+        return String.format("%s:%s", throwable.getClass().getCanonicalName(), throwable.getMessage());
+    }
+
+    public static String formatGuildLog(Guild guild) {
+        return String.format("**Owner:** %s\n**Created:** %s\n**Members:** `%s`\n**Identifier:** `%s`", guild.getMembers().size(), guild.getCreationTime().format(DateTimeFormatter.ISO_LOCAL_DATE).replaceAll("-", "."), guild.getOwner().getAsMention(), guild.getId());
+    }
+
+    public static String formatMemberLog(Member member) {
+        return String.format("**Mention:** %s\n**Joined:** %s\n**Count:** `#%s`\n**Identifier:** `%s`", member.getAsMention(), member.getJoinDate().format(DateTimeFormatter.ISO_LOCAL_DATE).replaceAll("-", "."), member.getGuild().getMembers().size(), member.getUser().getId());
     }
 }
