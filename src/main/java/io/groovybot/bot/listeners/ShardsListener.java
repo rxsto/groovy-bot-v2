6f8073c4aaa8e26e7cc6210b7f78fa13d4d9ff4b
Oskar Lang <oskar.risto.lang@gmail.com>
2018-11-02 23:57:05 +0100

    Added Logger

--- src/main/java/io/groovybot/bot/listeners/ShardsListener.java
+++ src/main/java/io/groovybot/bot/listeners/ShardsListener.java
@@ -2,21 +2,29 @@ package io.groovybot.bot.listeners;
 
 import io.groovybot.bot.GroovyBot;
 import io.groovybot.bot.core.events.bot.AllShardsLoadedEvent;
+import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.bot.sharding.ShardManager;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.SubscribeEvent;
 
+@Log4j2
 public class ShardsListener {
 
     private int tempLoadedShards = 0;
 
-
     @SubscribeEvent
     @SuppressWarnings("unused")
     private void onReady(ReadyEvent event) {
-        tempLoadedShards++;
         ShardManager shardManager = GroovyBot.getInstance().getShardManager();
-        if (tempLoadedShards == shardManager.getShardsTotal())
+
+        tempLoadedShards++;
+
+        log.info(String.format("[ShardManager] Successfully launched Shard %s! (%s/%s)", event.getJDA().getShardInfo().getShardId(), tempLoadedShards, shardManager.getShardsTotal()));
+
+        if (tempLoadedShards == shardManager.getShardsTotal()) {
+            log.info(String.format("[ShardManager] Successfully launched %s Shards! Finishing Startup ...", tempLoadedShards));
+
             GroovyBot.getInstance().getEventManager().handle(new AllShardsLoadedEvent(event.getJDA(), event.getResponseNumber()));
+        }
     }
 }
