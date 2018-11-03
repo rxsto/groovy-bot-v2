18badfcfc14d8a69d62491a7494bd2f6500956e4
Oskar Lang <oskar.risto.lang@gmail.com>
2018-11-03 13:27:36 +0100

    Changed PartnerCommand structure

--- src/main/java/io/groovybot/bot/commands/general/PartnerCommand.java
+++ src/main/java/io/groovybot/bot/commands/general/PartnerCommand.java
@@ -8,7 +8,7 @@ import io.groovybot.bot.core.command.permission.Permissions;
 
 public class PartnerCommand extends Command {
     public PartnerCommand() {
-        super(new String[]{"partner", "partners"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you Groovy's partnes", "");
+        super(new String[]{"partner"}, CommandCategory.GENERAL, Permissions.everyone(), "Shows you Groovy's partne", "");
     }
 
     @Override
