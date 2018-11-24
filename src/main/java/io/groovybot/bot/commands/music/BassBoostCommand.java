ce9b51630027595c6b4c686ebb3d3343531b7114
Oskar Lang <oskar.risto.lang@gmail.com>
2018-11-24 23:36:38 +0100

    Upgraded BassBoostCommand

--- src/main/java/io/groovybot/bot/commands/music/BassBoostCommand.java
+++ src/main/java/io/groovybot/bot/commands/music/BassBoostCommand.java
@@ -10,42 +10,40 @@ import io.groovybot.bot.core.command.voice.SameChannelCommand;
 public class BassBoostCommand extends SameChannelCommand {
 
     public BassBoostCommand() {
-        super(new String[]{"bassboost", "bb"}, CommandCategory.MUSIC, Permissions.tierOne(), "Bass boosts songs.", "[off/low/medium/high/extreme]");
+        super(new String[]{"bassboost", "bb"}, CommandCategory.MUSIC, Permissions.tierOne(), "Lets you toggle the bassboost", "[off/low/medium/high/extreme]");
     }
 
     @Override
     public Result runCommand(String[] args, CommandEvent event, MusicPlayer player) {
         if(args.length == 0)
-            return sendHelp();
+            return send(success(event.translate("command.bassboost.info.title"), String.format(event.translate("command.bassboost.info.description"), player.getBassboost())));
         float[] bands = new float[15];
-        Result result = null;
         switch (args[0]) {
             case "off":
                 bands[0] = 0f;
                 bands[1] = 0f;
-                result = send(success(event.translate("command.bassboost.off.title"), event.translate("command.bassboost.off.description")));
                 break;
             case "low":
                 bands[0] = .25f;
                 bands[1] = .15f;
-                result = send(success(event.translate("command.bassboost.on.title"), event.translate("command.bassboost.low.description")));
                 break;
             case "medium":
                 bands[0] = .5f;
                 bands[1] = .25f;
-                result = send(success(event.translate("command.bassboost.on.title"), event.translate("command.bassboost.medium.description")));
                 break;
             case "high":
                 bands[0] = .75f;
                 bands[1] = .5f;
-                result = send(success(event.translate("command.bassboost.on.title"), event.translate("command.bassboost.high.description")));
                 break;
             case "extreme":
                 bands[0] = 1f;
                 bands[1] = .75f;
-                result = send(success(event.translate("command.bassboost.on.title"), event.translate("command.bassboost.extreme.description")));
+                break;
+            default:
+                return sendHelp();
         }
         player.getPlayer().getEqualizer().setGain(bands);
-        return result;
+        player.setBassboost(args[0]);
+        return send(success(event.translate("command.bassboost.title"), String.format(event.translate("command.bassboost.description"), args[0])));
     }
 }
