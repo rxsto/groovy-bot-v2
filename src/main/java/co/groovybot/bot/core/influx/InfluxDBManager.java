68116111ba74c9d04fd9c3a523e2c6941713ceec
Stupreme <justuskliem@gmail.com>
2018-12-21 14:01:08 +0100

    Only deploy on develop branch

--- src/main/java/co/groovybot/bot/core/influx/InfluxDBManager.java
+++ src/main/java/co/groovybot/bot/core/influx/InfluxDBManager.java
@@ -1,6 +1,5 @@
 package co.groovybot.bot.core.influx;
 
-import co.groovybot.bot.GroovyBot;
 import co.groovybot.bot.io.config.Configuration;
 import lombok.extern.log4j.Log4j2;
 import org.influxdb.InfluxDB;
