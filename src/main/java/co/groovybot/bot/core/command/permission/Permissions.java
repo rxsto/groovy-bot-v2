cb33b7cc5914dc6bea3909ee613acdd088654280
Oskar Lang <oskar.risto.lang@gmail.com>
2018-12-22 14:01:01 +0100

    Removed Debugging

--- src/main/java/co/groovybot/bot/core/command/permission/Permissions.java
+++ src/main/java/co/groovybot/bot/core/command/permission/Permissions.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Log4j2
@@ -102,8 +104,6 @@ public class Permissions {
     }
 
     public Boolean isCovered(UserPermissions permissions, CommandEvent event) {
-        if (permissions.getUser().getEntityId() == 207500411907735552L)
-            return false;
         if (permissions.getIsOwner())
             return true;
         if (everyone)
@@ -124,7 +124,6 @@ public class Permissions {
             return permissions.isDj(event.getGuild());
         return false;
     }
-
 
     private boolean isPremiumGuild(Guild guild) {
         try (Connection connection = GroovyBot.getInstance().getPostgreSQL().getDataSource().getConnection()) {
