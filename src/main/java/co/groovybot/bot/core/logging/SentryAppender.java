b85ff88e36ed22e6bdb62539009488240bc4d70c
Oskar Lang <oskar.risto.lang@gmail.com>
2019-01-16 21:30:48 +0100

    Finished Sentry

--- src/main/java/co/groovybot/bot/core/logging/SentryAppender.java
+++ src/main/java/co/groovybot/bot/core/logging/SentryAppender.java
@@ -1,7 +1,11 @@
 package co.groovybot.bot.core.logging;
 
 import io.sentry.Sentry;
+import io.sentry.event.Event;
+import io.sentry.event.EventBuilder;
+import io.sentry.event.interfaces.ExceptionInterface;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.Core;
 import org.apache.logging.log4j.core.Filter;
@@ -11,6 +15,8 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+
+import java.util.Date;
 
 @SuppressWarnings("unused")
 @Log4j2
@@ -29,21 +35,24 @@ public class SentryAppender extends AbstractAppender {
     @Override
     public void append(LogEvent event) {
         if (event.getLevel().name().equalsIgnoreCase("error") || event.getLevel().name().equalsIgnoreCase("warn") || event.getLevel().name().equalsIgnoreCase("fatal")) {
-            Sentry.getContext().addExtra("message", event.getMessage().getFormattedMessage());
-            Sentry.getContext().addExtra("level", event.getLevel().name());
-            Sentry.getContext().addExtra("thread-info", String.format("%s (%s)", event.getThreadName(), event.getThreadId()));
+            EventBuilder eventBuilder = new EventBuilder()
+                    .withTimestamp(new Date())
+                    .withMessage(event.getMessage().getFormattedMessage())
+                    .withExtra("level", event.getLevel().name())
+                    .withExtra("thread-info", String.format("%s (%s)", event.getThreadName(), event.getThreadId()));
+
+            if (event.getLevel().equals(Level.WARN))
+                eventBuilder.withLevel(Event.Level.WARNING);
+            else
+                eventBuilder.withLevel(Event.Level.valueOf(event.getLevel().name()));
 
             if (event.getSource() != null)
-                Sentry.getContext().addExtra("source", event.getSource().toString());
+                eventBuilder.withLogger(event.getSource().getClassName());
 
             if (event.getThrown() != null)
-                Sentry.capture(event.getThrown());
-            else
-                Sentry.capture(event.getMessage().getFormattedMessage());
+                eventBuilder.withSentryInterface(new ExceptionInterface(event.getThrown()));
 
-            log.info("Successfully posted LogEvent to Sentry!");
-
-            Sentry.getContext().clear();
+            Sentry.capture(eventBuilder);
         }
     }
 }
